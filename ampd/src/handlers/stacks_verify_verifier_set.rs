314d702761712ba0af847bea4b2df7f9fecaf6d8
Rares <6453351+raress96@users.noreply.github.com>
2024-10-16 17:12:25 +0300

    Implement verify verifier set handler for stacks.

---
+++ ampd/src/handlers/stacks_verify_verifier_set.rs
@@ -0,0 +1,140 @@
+use std::convert::TryInto;
+
+use async_trait::async_trait;
+use axelar_wasm_std::voting::{PollId, Vote};
+use cosmrs::cosmwasm::MsgExecuteContract;
+use cosmrs::tx::Msg;
+use cosmrs::Any;
+use error_stack::ResultExt;
+use events::Error::EventTypeMismatch;
+use events::Event;
+use events_derive::try_from;
+use multisig::verifier_set::VerifierSet;
+use serde::Deserialize;
+use tokio::sync::watch::Receiver;
+use tracing::{info, info_span};
+use valuable::Valuable;
+use voting_verifier::msg::ExecuteMsg;
+
+use crate::event_processor::EventHandler;
+use crate::handlers::errors::Error;
+use crate::stacks::http_client::Client;
+use crate::stacks::verifier::verify_verifier_set;
+use crate::types::{Hash, TMAddress};
+
+#[derive(Deserialize, Debug)]
+pub struct VerifierSetConfirmation {
+    pub tx_id: Hash,
+    pub event_index: u32,
+    pub verifier_set: VerifierSet,
+}
+
+#[derive(Deserialize, Debug)]
+#[try_from("wasm-verifier_set_poll_started")]
+struct PollStartedEvent {
+    poll_id: PollId,
+    source_gateway_address: String,
+    verifier_set: VerifierSetConfirmation,
+    participants: Vec<TMAddress>,
+    expires_at: u64,
+}
+
+pub struct Handler {
+    verifier: TMAddress,
+    voting_verifier_contract: TMAddress,
+    http_client: Client,
+    latest_block_height: Receiver<u64>,
+}
+
+impl Handler {
+    pub fn new(
+        verifier: TMAddress,
+        voting_verifier_contract: TMAddress,
+        http_client: Client,
+        latest_block_height: Receiver<u64>,
+    ) -> Self {
+        Self {
+            verifier,
+            voting_verifier_contract,
+            http_client,
+            latest_block_height,
+        }
+    }
+
+    fn vote_msg(&self, poll_id: PollId, vote: Vote) -> MsgExecuteContract {
+        MsgExecuteContract {
+            sender: self.verifier.as_ref().clone(),
+            contract: self.voting_verifier_contract.as_ref().clone(),
+            msg: serde_json::to_vec(&ExecuteMsg::Vote {
+                poll_id,
+                votes: vec![vote],
+            })
+            .expect("vote msg should serialize"),
+            funds: vec![],
+        }
+    }
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    type Err = Error;
+
+    async fn handle(&self, event: &Event) -> error_stack::Result<Vec<Any>, Error> {
+        if !event.is_from_contract(self.voting_verifier_contract.as_ref()) {
+            return Ok(vec![]);
+        }
+
+        let PollStartedEvent {
+            poll_id,
+            source_gateway_address,
+            verifier_set,
+            participants,
+            expires_at,
+            ..
+        } = match event.try_into() as error_stack::Result<_, _> {
+            Err(report) if matches!(report.current_context(), EventTypeMismatch(_)) => {
+                return Ok(vec![]);
+            }
+            event => event.change_context(Error::DeserializeEvent)?,
+        };
+
+        if !participants.contains(&self.verifier) {
+            return Ok(vec![]);
+        }
+
+        let latest_block_height = *self.latest_block_height.borrow();
+        if latest_block_height >= expires_at {
+            info!(poll_id = poll_id.to_string(), "skipping expired poll");
+            return Ok(vec![]);
+        }
+
+        let transaction = self
+            .http_client
+            .get_valid_transaction(&verifier_set.tx_id)
+            .await;
+
+        let vote = info_span!(
+            "verify a new verifier set for Stacks",
+            poll_id = poll_id.to_string(),
+            id = format!("{}_{}", verifier_set.tx_id, verifier_set.event_index)
+        )
+        .in_scope(|| {
+            info!("ready to verify a new worker set in poll");
+
+            let vote = transaction.map_or(Vote::NotFound, |transaction| {
+                verify_verifier_set(&source_gateway_address, &transaction, verifier_set)
+            });
+            info!(
+                vote = vote.as_value(),
+                "ready to vote for a new worker set in poll"
+            );
+
+            vote
+        });
+
+        Ok(vec![self
+            .vote_msg(poll_id, vote)
+            .into_any()
+            .expect("vote msg should serialize")])
+    }
+}
